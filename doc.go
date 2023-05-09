c92cd6f4ad6801426f4ec9526bcc5cb3c70295b9
Ryan Morton <rmorton@zerofox.com>
2023-05-09 10:21:54 -0400

    chore: Add support for Github Actions
    - Github Actions yaml for test + lint
    - README w/ go-doc reference link
    - doc.go for go docs
    - go mod init

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Open Asset Model is a comprehensive set of
+// data structures that can be used to structure and transmit
+// information about an organizations digital and physical assets.
+
+package open_asset_model
