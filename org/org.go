46911eee600a5d25ba56345d5a0e26fd711de91e
caffix <caffix@users.noreply.github.com>
2025-04-19 21:29:29 -0400

    added the Global Industry Classification Standard taxonomy map

--- org/org.go
+++ org/org.go
@@ -58,3 +58,110 @@ func (o Organization) AssetType() model.AssetType {
 func (o Organization) JSON() ([]byte, error) {
 	return json.Marshal(o)
 }
+
+// GICSIndustryMap captures the sectors, industry groups, and industries
+// of the Global Industry Classification Standard (GICS) taxonomy.
+// It is a standardized classification system for categorizing companies
+// into sectors and industries based on their primary business activities.
+// GICS was developed by MSCI and Standard & Poor's (S&P) to provide a
+// consistent framework for analyzing and comparing companies across
+// different sectors and industries. The GICS taxonomy is widely used by
+// investors, analysts, and financial professionals to assess the
+// performance of various sectors and industries in the global economy.
+// The GICS taxonomy consists of 11 sectors, each of which is further
+// divided into 24 industry groups, 69 industries, and 158 sub-industries.
+var GICSIndustryMap = map[string]map[string][]string{
+	"Communication Services": {
+		"Telecommunication Services": {"Diversified Telecommunication Services", "Wireless Telecommunication Services"},
+		"Media & Entertainment":      {"Media", "Entertainment", "Interactive Media & Services"},
+	},
+	"Consumer Discretionary": {
+		"Automobiles & Components": {"Automobiles", "Automobile Components"},
+		"Consumer Durable & Apparel": {
+			"Household Durables",
+			"Leisure Products",
+			"Textiles, Apparel & Luxury Goods",
+		},
+		"Consumer Services":                            {"Hotels, Restaurants & Leisure", "Diversified Consumer Services"},
+		"Consumer Discretionary Distribution & Retail": {"Distributors", "Broadline Retail", "Specialty Retail"},
+	},
+	"Consumer Staples": {
+		"Consumer Staples Distribtuion & Retail": {"Consumer Staples Distribution & Retail"},
+	},
+	"Energy": {
+		"Energy": {"Energy Equipment & Services", "Oil, Gas & Consumable Fuels"},
+	},
+	"Financials": {
+		"Banks": {"Banks"},
+		"Financial Services": {
+			"Financial Services",
+			"Consumer Finance",
+			"Capital Markets",
+			"Mortgage Real Estate Investment Trusts",
+		},
+		"Insurance": {"Insurance"},
+	},
+	"Health Care": {
+		"Health Care Equipment & Services":               {"Health Care Equipment & Supplies", "Health Care Providers & Services"},
+		"Pharmaceuticals, Biotechnology & Life Sciences": {"Pharmaceuticals", "Biotechnology", "Life Sciences Tools & Services"},
+	},
+	"Industrials": {
+		"Capital Goods": {
+			"Aerospace & Defense",
+			"Building Products",
+			"Construction & Engineering",
+			"Electrical Equipment",
+			"Industrial Conglomerates",
+			"Machinery",
+			"Trading Companies & Distributors",
+		},
+		"Commercial & Professional Services": {"Commercial Services & Supplies", "Professional Services"},
+		"Transportation": {
+			"Air Freight & Logistics",
+			"Passenger Airlines",
+			"Marine Transportation",
+			"Ground Transportation",
+			"Transportation Infrastructure",
+		},
+	},
+	"Information Technology": {
+		"Software & Services": {"Software", "IT Services"},
+		"Technology Hardware & Equipment": {
+			"Technology Hardware, Storage & Peripherals",
+			"Electronic Equipment, Instruments & Components",
+			"Communications Equipment",
+		},
+		"Semiconductors & Semiconductor Equipment": {"Semiconductors & Semiconductor Equipment"},
+	},
+	"Materials": {
+		"Materials": {
+			"Chemicals",
+			"Construction Materials",
+			"Containers & Packaging",
+			"Metals & Mining",
+			"Paper & Forest Products",
+		},
+	},
+	"Real Estate": {
+		"Equity Real Estate Investment Trusts": {
+			"Diversified REITs",
+			"Industrial REITs",
+			"Hotel & Resort REITs",
+			"Office REITs",
+			"Health Care REITs",
+			"Residential REITs",
+			"Retail REITs",
+			"Specialized REITs",
+		},
+		"Real Estate Management & Development": {"Real Estate Management & Development"},
+	},
+	"Utilities": {
+		"Utilities": {
+			"Electric Utilities",
+			"Gas Utilities",
+			"Multi-Utilities",
+			"Water Utilities",
+			"Independent Power and Renewable Electricity Producers",
+		},
+	},
+}
